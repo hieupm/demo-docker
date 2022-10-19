4c5ec85a0763ca7f9cfcbaa0d42bc51b04717e6d
John Le <nhan.le@savvycomsoftware.com>
2022-10-19 15:27:43 +0700

    Import security lib

---
+++ src/main/java/com/example/demodocker/security/SecurityConfigurer.java
@@ -0,0 +1,8 @@
+package com.example.demodocker.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+}
