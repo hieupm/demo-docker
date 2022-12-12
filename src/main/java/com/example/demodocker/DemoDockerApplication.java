ae9d2f29d0ff18f3804a68ce5701b71a932dd00d
hieupm <hieupm061299@gmail.com>
2022-12-12 17:19:40 +0700

    Update db config

--- src/main/java/com/example/demodocker/DemoDockerApplication.java
+++ src/main/java/com/example/demodocker/DemoDockerApplication.java
@@ -1,13 +1,9 @@
 package com.example.demodocker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class DemoDockerApplication {
 
     public static void main(String[] args) {
