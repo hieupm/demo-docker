2873fc5c98aa5d5b1bfaa196a8b39aabccd8fd21
john.da <johnda@twendeesoft.com>
2023-01-10 10:59:32 +0700

    Test git config username + email

--- src/main/java/com/example/demodocker/controller/CategoryController.java
+++ src/main/java/com/example/demodocker/controller/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Scanner;
 
 @RestController
 @RequestMapping(value = "/category")
@@ -55,5 +56,24 @@ public class CategoryController {
             return null;
         }
     }
-    //a
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+        if(N<1 || N>100) return;
+        if(N % 2 == 0){
+            if((N >= 2 && N <= 5) || N >= 20){
+                System.out.println("Not Weird");
+            } else if(N >= 6 && N <= 20){
+                System.out.println("Weird");
+            }
+
+        } else {
+            System.out.println("Weird");
+        }
+    }
+
 }
